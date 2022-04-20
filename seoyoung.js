b603a3eab41be55b3cced8187e1de9163fe47705
seo-young-k <77436293+seo-young-k@users.noreply.github.com>
2022-04-20 16:17:48 +0900

    Add files via upload

---
+++ seoyoung.js
@@ -0,0 +1,41 @@
+var num = 3;
+console.log(num);
+
+let str3 = 'a';
+console.log(str);
+
+const constant = 10;
+console.log(constant);
+
+let num = 10;
+console.log(num);
+
+const number = 10; 
+console.log(number);
+
+let userName = 'seo young';
+
+let number =1;
+let str4 ='abc';
+let bool = true;
+let undi = undefined;
+let nul = null;
+let symbol = symbol();
+
+console.log(typeof('abc'));
+
+let array =[];
+let obj = {};
+let func = function(){};
+
+console.log(typeof([]));
+
+let sum = 0.1 + 0.2;
+console.log(sum);
+
+let str = "abc";
+let str2 = 'def';
+console.log(str,str2);
+
+let str5 = "I'm fine thank you!\"and you?\"";
+
